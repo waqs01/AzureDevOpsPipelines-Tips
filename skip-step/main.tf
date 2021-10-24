afa84155f4e65d25b83ed54943648a01bee78eea
waqs01 <43854253+waqs01@users.noreply.github.com>
2021-10-24 22:29:02 +0100

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,15 +1,1 @@
-terraform {
-  backend "azurerm" {
-  }
-}
 
-provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-4"
-  location = "westeurope"
-}
