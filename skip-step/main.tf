b7285ec6e7a0859a0804f8466747b77a325f7641
waqs01 <43854253+waqs01@users.noreply.github.com>
2022-02-05 03:01:57 +0000

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -14,3 +14,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "resourcegroup-test-tbd-5"
   location = "westeurope"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resourcegroup-test-tbd-7"
+  location = "westeurope"
+}
